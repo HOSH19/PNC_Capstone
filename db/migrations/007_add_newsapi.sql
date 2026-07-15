6374dd5940c4bdbce4591e1087bdbe2d8c4fae11
mistyeyee <ritaruotongxu@gmail.com>
2026-07-15 14:58:58 -0700

    migration step

---
+++ db/migrations/007_add_newsapi.sql
@@ -0,0 +1,9 @@
+
+
+
+-- Allow newsapi source into raw_item.
+-- Pattern documented in db/migrations/002_raw_item.sql header.
+
+ALTER TABLE raw_item DROP CONSTRAINT IF EXISTS raw_item_source_check;
+ALTER TABLE raw_item ADD CONSTRAINT raw_item_source_check
+    CHECK (source IN ('gdelt', 'edgar', 'agency_rss', 'fed_enforcement', 'newsapi'));
