36260ef04a9dbadc0a1d15150e2ae732ebc629f6
Yusheng <yushengfeng@g.ucla.edu>
2026-07-24 16:03:32 -0700

    Update OCC migration checksum

--- db/migrations/012_add_occ_enforcement.sql
+++ db/migrations/012_add_occ_enforcement.sql
@@ -1,13 +1,15 @@
 ALTER TABLE raw_item DROP CONSTRAINT IF EXISTS raw_item_source_check;
 
 ALTER TABLE raw_item ADD CONSTRAINT raw_item_source_check
-CHECK (source IN (
-    'gdelt',
-    'edgar',
-    'fdic_enforcement',
-    'fed_enforcement',
-    'agency_rss',
-    'alpha_vantage',
-    'newsapi',
-    'occ_enforcement'
-));
+CHECK (
+    source IN (
+        'gdelt',
+        'edgar',
+        'fdic_enforcement',
+        'fed_enforcement',
+        'agency_rss',
+        'alpha_vantage',
+        'newsapi',
+        'occ_enforcement'
+    )
+);
